1a02f89d5c20f4fb7e9ac30203a8865ee6eae45f
cosdealpha <bocangelcamila@gmail.com>
2024-11-26 08:49:29 -0400

    Update target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.debuggers"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.debuggers"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
