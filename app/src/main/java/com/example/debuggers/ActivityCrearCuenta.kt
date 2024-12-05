3d2755cef6238d74a9cfb84dee609e31e4164388
cosdealpha <bocangelcamila@gmail.com>
2024-12-04 22:21:46 -0400

    correcionesBindingYStrings

--- app/src/main/java/com/example/debuggers/ActivityCrearCuenta.kt
+++ app/src/main/java/com/example/debuggers/ActivityCrearCuenta.kt
@@ -10,13 +10,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.debuggers.databinding.ActivityCrearCuentaBinding
+import com.example.debuggers.databinding.ActivityLoginBinding
 
 class ActivityCrearCuenta : AppCompatActivity() {
     private lateinit var binding: ActivityCrearCuentaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_crear_cuenta)
+        binding = ActivityCrearCuentaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/debuggers/ActivityLogin.kt
+++ app/src/main/java/com/example/debuggers/ActivityLogin.kt
@@ -15,8 +15,9 @@ class ActivityLogin : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
